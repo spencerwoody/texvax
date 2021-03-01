a628b5ccdd8dc1abb743395bbf2a16d2a9401f64
Spencer Woody <spencer.woody@utexas.edu>
2021-03-01 16:08:48 -0600

    Fix date

--- getdata.R
+++ getdata.R
@@ -6,6 +6,6 @@ library(lubridate)
 
 URL <- "https://www.dshs.texas.gov/immunize/covid19/COVID-19-Vaccine-Data-by-County.xls"
 
-myfilename <- paste0(here::here(), "/data/", now(), " COVID-19 Vaccine Data by County.xlsx")
+myfilename <- paste0(here::here(), "/data/", today(), " COVID-19 Vaccine Data by County.xlsx")
 
 download.file(URL, destfile = myfilename)
