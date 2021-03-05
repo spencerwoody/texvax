9c6b23b1917598d533591d2ffc718e07278559de
Spencer Woody <spencer.woody@utexas.edu>
2021-03-05 09:44:07 -0600

    Update for Mar 05, adding in ZIP code data

--- getdata.R
+++ getdata.R
@@ -9,3 +9,10 @@ URL <- "https://www.dshs.texas.gov/immunize/covid19/COVID-19-Vaccine-Data-by-Cou
 myfilename <- paste0(here::here(), "/data/", today(), " COVID-19 Vaccine Data by County.xlsx")
 
 download.file(URL, destfile = myfilename)
+
+
+URL_zip <- "https://dshs.texas.gov/coronavirus/TexasCOVID19VaccinesbyZIP.xlsx"
+
+myfilename_zip <- paste0(here::here(), "/data_zip/", today(), " COVID-19 Vaccine Data by ZIP.xlsx")
+
+download.file(URL_zip, destfile = myfilename_zip)
